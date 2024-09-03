025e946180466d0d8912eb6eddbfdc76cfa72e42
mtyszczak <mateusz.tyszczak@gmail.com>
2024-09-03 12:22:46 +0200

    Fix no namespace option given generating wrong code

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,5 +1,5 @@
 export const addNamespace = (obj: Record<string, any>, namespace?: string): Record<string, any> => {
-  if (namespace !== undefined || namespace.length > 0)
+  if (namespace !== undefined && namespace.length > 0)
     return {
       [namespace]: obj
     };
